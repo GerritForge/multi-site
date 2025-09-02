f401e2f2e0609b3170c321ad5e1dfdcf35d04052
Antonio Barone <syntonyze@gmail.com>
2025-09-02 11:05:51 +0200

    Rename ForwardedEventHandler to ForwardedEventDispatcher
    Renames the `ForwardedEventHandler` class and all of its usages to
    `ForwardedEventDispatcher` to better reflect its actual responsibility.
    
    Despite its previous name, the class does not handle stream events but
    instead dispatches them to the internal Gerrit event bus.
    
    This renaming improves code clarity by aligning the class name with its
    true role, as described in its documentation. It also prepares the
    codebase for the introduction of new handler classes that will take on
    actual event handling responsibilities, separate from internal
    dispatching.
    
    All relevant imports, variables, and test cases have been updated to use
    the new class name.
    
    Change-Id: Ib52857c06051d4d789e6b939ce8e3691979035db

--- src/test/java/com/googlesource/gerrit/plugins/multisite/event/StreamEventRouterTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/multisite/event/StreamEventRouterTest.java
@@ -21,7 +21,7 @@ import com.google.gerrit.entities.BranchNameKey;
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.server.events.CommentAddedEvent;
 import com.google.gerrit.server.util.time.TimeUtil;
-import com.googlesource.gerrit.plugins.multisite.forwarder.ForwardedEventHandler;
+import com.googlesource.gerrit.plugins.multisite.forwarder.ForwardedEventDispatcher;
 import com.googlesource.gerrit.plugins.multisite.forwarder.router.IndexEventRouter;
 import com.googlesource.gerrit.plugins.multisite.forwarder.router.StreamEventRouter;
 import org.junit.Before;
@@ -34,7 +34,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class StreamEventRouterTest {
 
   private StreamEventRouter router;
-  @Mock private ForwardedEventHandler streamEventHandler;
+  @Mock private ForwardedEventDispatcher streamEventHandler;
   @Mock private IndexEventRouter indexEventRouter;
 
   @Before
