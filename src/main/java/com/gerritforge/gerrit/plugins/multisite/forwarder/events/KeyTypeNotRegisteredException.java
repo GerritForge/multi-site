5e415018e419d5c344af6b8e61cd0a44453fc029
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-18 17:10:18 +0000

    Enable encoding/decoding of complex key types for CacheEvictionEvent
    By safely forwarding the type of the key that we're encoding, we enable
    multi-site to correctly encode/decode complex key types.
    
    This is important when processing cache-eviction events, as if we don't
    decode the key to the correct type, the comparison at eviction time will
    fail, effectively failing the removal of cache entries which is the sole
    purpose of this event.
    
    Bug: Issue 465491615
    Change-Id: Id85a3e6750c170540d80f1bcc915bcad72e4edd3

---
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/events/KeyTypeNotRegisteredException.java
@@ -0,0 +1,20 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.multisite.forwarder.events;
+
+public class KeyTypeNotRegisteredException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public KeyTypeNotRegisteredException(String keyType) {
+    super(String.format("Key type '%s' is not a registered type.", keyType));
+  }
+}
