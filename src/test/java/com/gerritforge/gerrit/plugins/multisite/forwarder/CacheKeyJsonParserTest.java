6937f5cc4b40a4c5b0edff11c87bb854cb06e8c1
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-18 17:10:18 +0000

    Test that Gson encoding/decoding works for complex types
    Introduce a new test for ensuring that Gson encoding/decoding
    is able to treat complex types and does not introduce any data
    manipulation.
    
    Bug: Issue 465491615
    Change-Id: Id85a3e6750c170540d80f1bcc915bcad72e4edd3

--- src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
@@ -14,9 +14,12 @@ package com.gerritforge.gerrit.plugins.multisite.forwarder;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.gerritforge.gerrit.plugins.multisite.cache.Constants;
+import com.gerritforge.gerrit.plugins.multisite.forwarder.events.CacheEvictionEvent;
+import com.gerritforge.gerrit.plugins.multisite.forwarder.events.MultiSiteEvent;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventGsonProvider;
 import com.google.gson.Gson;
 import org.junit.Test;
@@ -26,6 +29,25 @@ public class CacheKeyJsonParserTest {
 
   private final Gson gson = new EventGsonProvider().get();
   private final CacheKeyJsonParser gsonParser = new CacheKeyJsonParser(gson);
+
+  public record ComplexKeyType(String key) { }
+
+  @Test
+  public void serializeDeserializeCacheEvictionEventWithComplexKeyType() {
+    MultiSiteEvent.registerEventTypes();
+    CacheEvictionEvent event = new CacheEvictionEvent("test-cache", new ComplexKeyType("cache-key"), "myinstance");
+    String jsonEvent = gson.toJson(event);
+    Event parsedEvent = gson.fromJson(jsonEvent, Event.class);
+    assertThat(parsedEvent).isEqualTo(event);
+  }
+
+  @Test
+  public void serializeDeserializeCacheEvictionWithPrimitiveType() {
+    MultiSiteEvent.registerEventTypes();
+    CacheEvictionEvent event = new CacheEvictionEvent("test-cache", "cache-key", "myinstance");
+    String jsonEvent = gson.toJson(event);
+    assertThat(gson.fromJson(jsonEvent, CacheEvictionEvent.class)).isEqualTo(event);
+  }
 
   @Test
   public void accountIDParse() {
