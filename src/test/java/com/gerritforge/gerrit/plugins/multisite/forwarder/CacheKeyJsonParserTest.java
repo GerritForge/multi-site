49753c9824f2df391e1d380020908e12b2b4dce8
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-18 17:10:18 +0000

    Enable encoding/decoding of complex key types for CacheEvictionEvent
    Improve the Gson encoding/decoding so that its able to automatically
    treat complex key types and does not introduce any data manipulation.
    
    Bug: Issue 465491615
    Change-Id: Id85a3e6750c170540d80f1bcc915bcad72e4edd3

--- src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
@@ -12,20 +12,138 @@
 package com.gerritforge.gerrit.plugins.multisite.forwarder;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.server.events.EventTypes.register;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 
 import com.gerritforge.gerrit.plugins.multisite.cache.Constants;
+import com.gerritforge.gerrit.plugins.multisite.forwarder.events.CacheEvictionEvent;
+import com.gerritforge.gerrit.plugins.multisite.forwarder.events.MultiSiteEvent;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.client.DiffPreferencesInfo;
+import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventGsonProvider;
+import com.google.gerrit.server.patch.filediff.FileDiffCacheKey;
+import com.google.gerrit.server.patch.gitfilediff.GitFileDiffCacheImpl;
 import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CacheKeyJsonParserTest {
+  private static final String CACHE_NAME = "test-cache";
   private static final Object EMPTY_JSON = "{}";
 
   private final Gson gson = new EventGsonProvider().get();
   private final CacheKeyJsonParser gsonParser = new CacheKeyJsonParser(gson);
+
+  public static class ComplexKeyType extends MultiSiteEvent {
+    public static final String type = "aType";
+    public String aKey;
+
+    protected ComplexKeyType(String key, String instanceId) {
+      super(type, instanceId);
+      this.aKey = key;
+    }
+
+    @Override
+    public String toString() {
+      return String.format(
+          "CacheEvictionEvent { cacheName='%s', instanceId='%s', key=(%s) }",
+          CACHE_NAME, "myinstance", this.getClass().getName());
+    }
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    MultiSiteEvent.registerEventTypes();
+  }
+
+  @Test
+  public void serializeDeserializeCacheEvictionEventWithComplexKeyType() {
+    ComplexKeyType complexKeyType = new ComplexKeyType("cache-key", "myinstance");
+    register(complexKeyType.getClass().getName(), ComplexKeyType.class);
+    CacheEvictionEvent event =
+        new CacheEvictionEvent(CACHE_NAME, complexKeyType, complexKeyType.instanceId);
+    String jsonEvent = gson.toJson(event);
+    Event parsedEvent = gson.fromJson(jsonEvent, Event.class);
+    assertThat(parsedEvent).isInstanceOf(CacheEvictionEvent.class);
+    CacheEvictionEvent castedEvent = (CacheEvictionEvent) parsedEvent;
+    assertThat(castedEvent.key).isInstanceOf(ComplexKeyType.class);
+  }
+
+  @Test
+  public void serializeDeserializeCacheEvictionWithPrimitiveType() {
+    CacheEvictionEvent event = new CacheEvictionEvent(CACHE_NAME, "cache-key", "myinstance");
+    String jsonEvent = gson.toJson(event);
+    Event parsedEvent = gson.fromJson(jsonEvent, Event.class);
+    assertThat(parsedEvent).isEqualTo(event);
+  }
+
+  @Test
+  public void serializeDeserializeFileDiffCacheKey() {
+    FileDiffCacheKey fileDiffCacheKey =
+        FileDiffCacheKey.builder()
+            .project(Project.NameKey.parse("test"))
+            .oldCommit(ObjectId.zeroId())
+            .newCommit(ObjectId.zeroId())
+            .newFilePath("aFilePath")
+            .renameScore(1)
+            .diffAlgorithm(GitFileDiffCacheImpl.DiffAlgorithm.HISTOGRAM_WITH_FALLBACK_MYERS)
+            .whitespace(DiffPreferencesInfo.Whitespace.IGNORE_NONE)
+            .useTimeout(true)
+            .build();
+    CacheEvictionEvent event = new CacheEvictionEvent(CACHE_NAME, fileDiffCacheKey, "myinstance");
+    String jsonEvent = gson.toJson(event);
+    Event parsedEvent = gson.fromJson(jsonEvent, Event.class);
+    assertThat(parsedEvent).isEqualTo(event);
+  }
+
+  @Test
+  public void deserializeCacheEvictionSerializedWithoutKeyType() {
+    String oldEventString =
+        "{\n"
+            + "  \"cacheName\" : \"test-cache\","
+            + "  \"key\" : \"cache-key\","
+            + "  \"type\" : \"cache-eviction\","
+            + "  \"eventCreatedOn\" : 1767002059,"
+            + "  \"instanceId\" : \"myinstance\"}";
+
+    Event parsedEvent = gson.fromJson(oldEventString, Event.class);
+    assertThat(parsedEvent).isInstanceOf(CacheEvictionEvent.class);
+    CacheEvictionEvent event = (CacheEvictionEvent) parsedEvent;
+    assertThat(event.key).isInstanceOf(String.class);
+  }
+
+  @Test
+  public void failToDeserializeComplexKeyCacheEvictionSerializedWithoutKeyType() {
+    String oldEventString =
+        "{"
+            + "  \"cacheName\" : \"test-cache\","
+            + "  \"key\" : {\"someField\" : \"cache-key\"},"
+            + "  \"type\" : \"cache-eviction\","
+            + "  \"eventCreatedOn\" : 1767010101,"
+            + "  \"instanceId\" : \"myinstance\"}";
+
+    assertThrows(JsonParseException.class, () -> gson.fromJson(oldEventString, Event.class));
+  }
+
+  @Test
+  public void failToDeserializeCacheEvictionWithUnknownKeyType() {
+    String oldEventString =
+        "{"
+            + "  \"cacheName\" : \"test-cache\","
+            + "  \"key\" : {"
+            + "    \"keyValue\" : {\"aKey\" : \"cache-key\"},"
+            + "    \"keyClassName\" : \"unknownKeyType\"},"
+            + "  \"type\" : \"cache-eviction\","
+            + "  \"eventCreatedOn\" : 1767018518,"
+            + "  \"instanceId\" : \"myinstance\"}";
+
+    assertThrows(JsonParseException.class, () -> gson.fromJson(oldEventString, Event.class));
+  }
 
   @Test
   public void accountIDParse() {
