d6f18a4665e8509b03dd64b812c0093bba3bd680
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-18 17:10:18 +0000

    Enable encoding/decoding of complex key types for CacheEvictionEvent
    Improve the Gson encoding/decoding so that its able to automatically
    treat complex key types and does not introduce any data manipulation.
    
    Bug: Issue 465491615
    Change-Id: Id85a3e6750c170540d80f1bcc915bcad72e4edd3

---
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/events/KeyObject.java
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.multisite.forwarder.events;
+
+/**
+ * Object used to encapsulate cache keys and their type so that when deserializing cache eviction
+ * events we can be sure of their type.
+ */
+public class KeyObject {
+
+  public Object key;
+  public String keyType;
+
+  public KeyObject(Object key) {
+    this.key = key;
+    this.keyType = key.getClass().getName();
+  }
+}
