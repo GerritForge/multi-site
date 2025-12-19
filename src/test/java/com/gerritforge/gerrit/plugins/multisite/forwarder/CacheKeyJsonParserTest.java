f1d0a541a1e078cc60c968e256b60ace2a7fc81e
Daniele Sassoli <danielesassoli@gmail.com>
2025-12-19 16:22:40 +0000

    Remove CacheKeyJsonParser
    Change-Id: I454a4d2895d62a56a393775b1514340e51b5f0e6

--- src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
@@ -13,7 +13,6 @@ package com.gerritforge.gerrit.plugins.multisite.forwarder;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.gerritforge.gerrit.plugins.multisite.cache.Constants;
 import com.gerritforge.gerrit.plugins.multisite.forwarder.events.CacheEvictionEvent;
 import com.gerritforge.gerrit.plugins.multisite.forwarder.events.MultiSiteEvent;
 import com.google.gerrit.entities.Account;
@@ -28,7 +27,6 @@ public class CacheKeyJsonParserTest {
   private static final Object EMPTY_JSON = "{}";
 
   private final Gson gson = new EventGsonProvider().get();
-  private final CacheKeyJsonParser gsonParser = new CacheKeyJsonParser(gson);
 
   public record ComplexKeyType(String key) { }
 
@@ -54,34 +52,28 @@ public class CacheKeyJsonParserTest {
   public void accountIDParse() {
     Account.Id accountId = Account.id(1);
     String json = gson.toJson(accountId);
-    assertThat(accountId).isEqualTo(gsonParser.from(Constants.ACCOUNTS, json));
+    assertThat(accountId).isEqualTo(gson.fromJson(json, Account.Id.class));
   }
 
   @Test
   public void accountGroupIDParse() {
     AccountGroup.Id accountGroupId = AccountGroup.id(1);
     String json = gson.toJson(accountGroupId);
-    assertThat(accountGroupId).isEqualTo(gsonParser.from(Constants.GROUPS, json));
+    assertThat(accountGroupId).isEqualTo(gson.fromJson(json, AccountGroup.Id.class));
   }
 
   @Test
   public void accountGroupUUIDParse() {
     AccountGroup.UUID accountGroupUuid = AccountGroup.uuid("abc123");
     String json = gson.toJson(accountGroupUuid);
-    assertThat(accountGroupUuid).isEqualTo(gsonParser.from(Constants.GROUPS_BYINCLUDE, json));
+    assertThat(accountGroupUuid).isEqualTo(gson.fromJson(json, AccountGroup.UUID.class));
   }
 
   @Test
   public void projectNameKeyParse() {
     String projectNameString = "foo";
     Project.NameKey projectNameKey = Project.nameKey(projectNameString);
-    assertThat(projectNameKey).isEqualTo(gsonParser.from(Constants.PROJECTS, projectNameString));
-  }
-
-  @Test
-  public void stringParse() {
-    String key = "key";
-    assertThat(key).isEqualTo(gsonParser.from("any-cache-with-string-key", key));
+    assertThat(projectNameKey).isEqualTo(gson.fromJson(projectNameString, Project.NameKey.class));
   }
 
   @Test
