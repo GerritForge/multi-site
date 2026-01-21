520d3c28ba17ea983ab562632c902471a2720d45
Daniele Sassoli <danielesassoli@gmail.com>
2026-01-21 11:54:38 +0000

    Leverage new CacheDef map to correctly infer cache key types
    As Gerrit now exposes a map of registered cache definitions, we can now
    automatically infer the key types from the cache name, allowing us to
    streamline the logic in the CacheKeyJsonParser.
    This has the added benefit that we,almost, no longer need to maintain a
    separate list of cache names and their types, as well as having to keep
    track of how to instantiate said types. This is now all deferred to
    Gson.
    
    We still need to maintain these mappings for GROUPS and GROUPS_BYSUBGROUP, as there is no type adapter in Gerrit core that
    allows us to automatically parse these to the correct type.
    
    Bug: Issue 465491615
    Change-Id: I09b3877d2ee6cdff2be933b3aa277ecdb881edd3

--- src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParser.java
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParser.java
@@ -16,17 +16,26 @@ import com.google.common.base.MoreObjects;
 import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.AccountGroup;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.server.cache.CacheDef;
 import com.google.gerrit.server.events.EventGson;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 import com.google.inject.Inject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class CacheKeyJsonParser {
   private final Gson gson;
+  private final DynamicMap<CacheDef<?, ?>> cachesMap;
 
   @Inject
-  public CacheKeyJsonParser(@EventGson Gson gson) {
+  public CacheKeyJsonParser(@EventGson Gson gson, DynamicMap<CacheDef<?, ?>> cachesMap) {
     this.gson = gson;
+    this.cachesMap = cachesMap;
   }
 
   @SuppressWarnings("cast")
@@ -53,24 +62,42 @@ public final class CacheKeyJsonParser {
         parsedKey = gson.fromJson(nullToEmpty(cacheKeyValue).toString(), Object.class);
         break;
       default:
-        if (cacheKeyValue instanceof String) {
-          parsedKey = (String) cacheKeyValue;
+        Map<String, Class<?>> cacheDefMap = getDynamicCacheDefs();
+        if (!cacheDefMap.containsKey(cacheName)) {
+          throw new IllegalStateException(cacheName);
+        }
+        Class<?> cls = cacheDefMap.get(cacheName);
+        JsonElement json = jsonElement(cacheKeyValue);
+        if (json.isJsonPrimitive()) {
+          parsedKey = gson.fromJson(json.getAsJsonPrimitive(), cls);
         } else {
-          try {
-            parsedKey = gson.fromJson(nullToEmpty(cacheKeyValue).toString().trim(), String.class);
-          } catch (Exception e) {
-            parsedKey = gson.fromJson(nullToEmpty(cacheKeyValue).toString(), Object.class);
-          }
+          parsedKey = gson.fromJson(json.getAsJsonObject(), cls);
         }
     }
     return parsedKey;
   }
 
   private JsonElement jsonElement(Object json) {
-    return gson.fromJson(nullToEmpty(json), JsonElement.class);
+    String nullToEmptyJson = nullToEmpty(json);
+    try {
+      return gson.fromJson(nullToEmptyJson, JsonElement.class);
+    } catch (JsonParseException e) {
+      return new JsonPrimitive(nullToEmptyJson);
+    }
   }
 
   private static String nullToEmpty(Object value) {
     return MoreObjects.firstNonNull(value, "").toString().trim();
+  }
+
+  private Map<String, Class<?>> getDynamicCacheDefs() {
+    Map<String, Class<?>> cacheDefMap = new HashMap<>();
+    for (String pluginName : cachesMap.plugins()) {
+      for (String cacheName : cachesMap.byPlugin(pluginName).keySet()) {
+        CacheDef<?, ?> cacheDef = cachesMap.byPlugin(pluginName).get(cacheName).get();
+        cacheDefMap.put(cacheDef.name(), cacheDef.keyType().getRawType());
+      }
+    }
+    return cacheDefMap;
   }
 }
