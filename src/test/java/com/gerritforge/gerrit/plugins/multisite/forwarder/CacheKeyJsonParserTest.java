3b9abea87d3927dae5f25b00d48641b03592d48c
Luca Milanesio <luca.milanesio@gmail.com>
2026-01-26 09:54:39 +0000

    Persistent caches management
    Persistent caches expose their own key serialiser that can and
    should be used to both serialise and deserialise the cache key values.
    
    Change-Id: Ibbbe2696411891a98a8c3c974e543336e36c3c78

--- src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/multisite/forwarder/CacheKeyJsonParserTest.java
@@ -25,15 +25,20 @@ import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.extensions.registration.PrivateInternals_DynamicMapImpl;
 import com.google.gerrit.extensions.registration.RegistrationHandle;
 import com.google.gerrit.server.cache.CacheDef;
+import com.google.gerrit.server.cache.PersistentCacheDef;
+import com.google.gerrit.server.cache.serialize.CacheSerializer;
+import com.google.gerrit.server.change.ChangeKindCacheImpl;
 import com.google.gerrit.server.events.EventGsonProvider;
 import com.google.gson.Gson;
 import com.google.inject.TypeLiteral;
 import com.google.inject.util.Providers;
 import java.time.Duration;
+import org.eclipse.jgit.lib.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CacheKeyJsonParserTest {
+  private static final String CACHE_CHANGE_KIND = "change_kind";
   private static final String CACHE_NAME_WITH_COMPLEX_KEY_TYPE = "complex-test-cache";
   private static final String CACHE_NAME_WITH_SIMPLE_KEY_TYPE = "simple-test-cache";
   private static final Object EMPTY_JSON = "{}";
@@ -53,10 +58,17 @@ public class CacheKeyJsonParserTest {
     cacheDefMap =
         (PrivateInternals_DynamicMapImpl<CacheDef<?, ?>>) DynamicMap.<CacheDef<?, ?>>emptyMap();
 
+    definePersistentCache(
+        GERRIT,
+        CACHE_CHANGE_KIND,
+        ChangeKindCacheImpl.Key.class,
+        new MockMultiSiteChangeKindCacheKeySerializer());
     defineCache(GERRIT, CACHE_NAME_WITH_COMPLEX_KEY_TYPE, ComplexKey.class);
     defineCache(GERRIT, CACHE_NAME_WITH_SIMPLE_KEY_TYPE, String.class);
     defineCache(PLUGIN_NAME1, CACHE_NAME_WITH_COMPLEX_KEY_TYPE, ComplexKey.class);
     defineCache(PLUGIN_NAME2, CACHE_NAME_WITH_COMPLEX_KEY_TYPE, ComplexKey.class);
+    defineCache(GERRIT, Constants.GROUPS, AccountGroup.Id.class);
+    defineCache(GERRIT, Constants.ACCOUNTS, Account.Id.class);
     gsonParser = new CacheKeyJsonParser(gson, cacheDefMap);
   }
 
@@ -68,21 +80,42 @@ public class CacheKeyJsonParserTest {
             Providers.of(new TestCacheDef<>(cacheName, keyRawType, Object.class)));
   }
 
+  private <K> void definePersistentCache(
+      String pluginName, String cacheName, Class<K> keyRawType, CacheSerializer<K> keySerializer) {
+    RegistrationHandle unused =
+        cacheDefMap.put(
+            pluginName,
+            cacheName,
+            Providers.of(
+                new TestPersistentCacheDef<>(cacheName, keyRawType, Object.class, keySerializer)));
+  }
+
   @Test
   public void serializeDeserializeCacheEvictionEventWithComplexKeyType() {
     ComplexKey complexKeyType = new ComplexKey("cache-key");
-    String jsonEvent = gson.toJson(complexKeyType);
+    String jsonEvent = gsonParser.toJson(CACHE_NAME_WITH_COMPLEX_KEY_TYPE, complexKeyType);
     Object parsedKey =
         gsonParser.from(CachePluginAndNameRecord.from(CACHE_NAME_WITH_COMPLEX_KEY_TYPE), jsonEvent);
     assertThat(parsedKey).isEqualTo(complexKeyType);
   }
 
   @Test
+  public void serializeDeserializeCacheEvictionEventWithAutoValueKeyType() {
+    ChangeKindCacheImpl.Key changeKindType =
+        ChangeKindCacheImpl.Key.create(ObjectId.fromString("3ac9c9cb5bc4b38606b06bc3df49d970ca0ce7b2"), ObjectId.fromString("b99e90c10f33d7c4c1eead0531119ac9417f8c78"), "sample-strategy");
+    String jsonEvent = gsonParser.toJson(CACHE_CHANGE_KIND, changeKindType);
+    Object parsedKey = gsonParser.from(CachePluginAndNameRecord.from(CACHE_CHANGE_KIND), jsonEvent);
+    assertThat(parsedKey).isEqualTo(changeKindType);
+  }
+
+  @Test
   public void serializeDeserializeCacheEvictionEventForMultiplePlugins() {
     ComplexKey complexKeyType1 = new ComplexKey("cache-key-1");
     ComplexKey complexKeyType2 = new ComplexKey("cache-key-2");
-    String jsonEvent1 = gson.toJson(complexKeyType1);
-    String jsonEvent2 = gson.toJson(complexKeyType2);
+    String jsonEvent1 =
+        gsonParser.toJson(PLUGIN_NAME1 + "." + CACHE_NAME_WITH_COMPLEX_KEY_TYPE, complexKeyType1);
+    String jsonEvent2 =
+        gsonParser.toJson(PLUGIN_NAME2 + "." + CACHE_NAME_WITH_COMPLEX_KEY_TYPE, complexKeyType2);
     Object parsedKey1 =
         gsonParser.from(
             CachePluginAndNameRecord.from(PLUGIN_NAME1 + "." + CACHE_NAME_WITH_COMPLEX_KEY_TYPE),
@@ -98,7 +131,7 @@ public class CacheKeyJsonParserTest {
   @Test
   public void accountIDParse() {
     Account.Id accountId = Account.id(1);
-    String json = gson.toJson(accountId);
+    String json = gsonParser.toJson(Constants.ACCOUNTS, accountId);
     assertThat(accountId)
         .isEqualTo(gsonParser.from(CachePluginAndNameRecord.from(Constants.ACCOUNTS), json));
   }
@@ -106,7 +139,7 @@ public class CacheKeyJsonParserTest {
   @Test
   public void accountGroupIDParse() {
     AccountGroup.Id accountGroupId = AccountGroup.id(1);
-    String json = gson.toJson(accountGroupId);
+    String json = gsonParser.toJson(Constants.GROUPS, accountGroupId);
     assertThat(accountGroupId)
         .isEqualTo(gsonParser.from(CachePluginAndNameRecord.from(Constants.GROUPS), json));
   }
@@ -114,7 +147,7 @@ public class CacheKeyJsonParserTest {
   @Test
   public void accountGroupUUIDParse() {
     AccountGroup.UUID accountGroupUuid = AccountGroup.uuid("abc123");
-    String json = gson.toJson(accountGroupUuid);
+    String json = gsonParser.toJson(CACHE_NAME_WITH_SIMPLE_KEY_TYPE, accountGroupUuid);
     assertThat(accountGroupUuid)
         .isEqualTo(
             gsonParser.from(CachePluginAndNameRecord.from(Constants.GROUPS_BYSUBGROUP), json));
@@ -140,8 +173,93 @@ public class CacheKeyJsonParserTest {
   @Test
   public void noKeyParse() {
     Object object = new Object();
-    String json = gson.toJson(object);
+    String json = gsonParser.toJson(CACHE_NAME_WITH_SIMPLE_KEY_TYPE, object);
     assertThat(json).isEqualTo(EMPTY_JSON);
+  }
+
+  private static class TestPersistentCacheDef<K, V> implements PersistentCacheDef<K, V> {
+    private final String name;
+    private final TypeLiteral<K> keyType;
+    private final TypeLiteral<V> valueType;
+    private final CacheSerializer<K> keySerializer;
+
+    TestPersistentCacheDef(
+        String name, Class<K> keyClass, Class<V> valueClass, CacheSerializer<K> keySerializer) {
+      this.name = name;
+      this.keyType = TypeLiteral.get(keyClass);
+      this.valueType = TypeLiteral.get(valueClass);
+      this.keySerializer = keySerializer;
+    }
+
+    @Override
+    public long diskLimit() {
+      return 0;
+    }
+
+    @Override
+    public int version() {
+      return 0;
+    }
+
+    @Override
+    public CacheSerializer<K> keySerializer() {
+      return keySerializer;
+    }
+
+    @Override
+    public CacheSerializer<V> valueSerializer() {
+      return null;
+    }
+
+    @Override
+    public String name() {
+      return "";
+    }
+
+    @Override
+    public String configKey() {
+      return "";
+    }
+
+    @Override
+    public TypeLiteral<K> keyType() {
+      return null;
+    }
+
+    @Override
+    public TypeLiteral<V> valueType() {
+      return null;
+    }
+
+    @Override
+    public long maximumWeight() {
+      return 0;
+    }
+
+    @Override
+    public Duration expireAfterWrite() {
+      return null;
+    }
+
+    @Override
+    public Duration expireFromMemoryAfterAccess() {
+      return null;
+    }
+
+    @Override
+    public Duration refreshAfterWrite() {
+      return null;
+    }
+
+    @Override
+    public Weigher<K, V> weigher() {
+      return null;
+    }
+
+    @Override
+    public CacheLoader<K, V> loader() {
+      return null;
+    }
   }
 
   private static class TestCacheDef<K, V> implements CacheDef<K, V> {
