72d1d5509a1eb4b9bdeada0e6554d9391eec0d5a
Daniele Sassoli <danielesassoli@gmail.com>
2026-04-27 18:01:49 +0100

    Throw exception if no projectName in changeIndexEvent
    Since I42bac21a, change numbers are no longer unique per installation,
    but rather per project. Trying to find a change number in the index
    could potentially lead to finding a change on a different project,
    therefore leading to a removal from the index of the wrong change
    entirely. If projectName cannot be found, throw an exception so that we
    risk incorrectly processing the message.
    
    Change-Id: I21ccf2e57598161fcb8e9c53af8ef191c4af767b

---
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/consumer/ProjectNameNotFoundException.java
@@ -0,0 +1,20 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.multisite.consumer;
+
+public class ProjectNameNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ProjectNameNotFoundException(int changeNumber) {
+    super(String.format("ProjectName for changeIndex event on change %s was empty", changeNumber));
+  }
+}
