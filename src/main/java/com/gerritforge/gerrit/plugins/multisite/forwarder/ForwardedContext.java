60b0fc3775a7e77d1cc39a1b89c836811042b733
Luca Milanesio <luca.milanesio@gmail.com>
2026-07-12 10:11:21 +0100

    Use AutoCloseable for managing forwarded context
    The use of the forwarded context thread-local relied on the manual
    set/unset, which was tedious to read, write and error-prone.
    
    Use the more natural AutoCloseable interface from Java and allow easier
    use throughout the code.
    
    Change-Id: I1b14fd439cc3a8986218e9b0caad0abaa53ca9e9

--- src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/ForwardedContext.java
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/forwarder/ForwardedContext.java
@@ -16,11 +16,24 @@ import static com.gerritforge.gerrit.plugins.replication.pull.api.PullReplicatio
 import static com.gerritforge.gerrit.plugins.replication.pull.api.PullReplicationEndpoints.APPLY_OBJECT_API_ENDPOINT;
 
 /** Allows to tag a forwarded event to avoid infinitely looping events. */
-public class Context {
+public class ForwardedContext implements AutoCloseable {
   public static final String PULL_REPLICATION_PLUGIN_NAME = "pull-replication";
   private static final ThreadLocal<Boolean> forwardedEvent = ThreadLocal.withInitial(() -> false);
 
-  private Context() {}
+  final boolean forwarded;
+
+  private ForwardedContext() {
+    forwarded = setForwardedEvent(true);
+  }
+
+    @Override
+    public void close() {
+      setForwardedEvent(forwarded);
+    }
+
+  public static ForwardedContext open() {
+    return new ForwardedContext();
+  }
 
   public static Boolean isForwardedEvent() {
     return forwardedEvent.get()
@@ -30,14 +43,10 @@ public class Context {
         isPullReplicationApplyObjectIndexing();
   }
 
-  public static Boolean setForwardedEvent(Boolean b) {
+  private Boolean setForwardedEvent(Boolean b) {
     Boolean forwarded = forwardedEvent.get();
     forwardedEvent.set(b);
     return forwarded;
-  }
-
-  public static void unsetForwardedEvent() {
-    forwardedEvent.remove();
   }
 
   public static boolean isPullReplicationApplyObjectIndexing() {
