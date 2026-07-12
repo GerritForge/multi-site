8d039d0cc032849e8e6e1b50296d8a529680e155
Luca Milanesio <luca.milanesio@gmail.com>
2026-07-12 23:14:25 +0100

    Implement UpToDate/ConsistencyChecker for AccountChecker
    Implement the support for consistency and up-to-date checking
    for the account index.
    
    This is part of the journey to have the
    ForwardedIndexingHandlerWithRetry to support all index types and
    checkers, without being specific to the change index.
    
    Change-Id: Ica4088e63e8e78285b44137374f89c009b2efb25

---
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/index/AccountCheckerImpl.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.multisite.index;
+
+import com.gerritforge.gerrit.plugins.multisite.forwarder.events.AccountIndexEvent;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.util.Optional;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+@Singleton
+public class AccountCheckerImpl implements AccountChecker {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final GitRepositoryManager repoManager;
+  private final AllUsersName allUsers;
+
+  @Inject
+  AccountCheckerImpl(GitRepositoryManager repoManager, AllUsersName allUsers) {
+    this.repoManager = repoManager;
+    this.allUsers = allUsers;
+  }
+
+  @Override
+  public boolean isUpToDate(Optional<AccountIndexEvent> event) {
+    if (event.isEmpty() || event.get().targetSha == null) {
+      logger.atWarning().log("Account index event has no target SHA; skipping the revision check");
+      return true;
+    }
+
+    AccountIndexEvent accountEvent = event.get();
+    try (Repository repo = repoManager.openRepository(allUsers);
+        RevWalk revWalk = new RevWalk(repo)) {
+      Ref accountRef = getAccountRef(repo, accountEvent.accountId);
+      RevCommit expected = revWalk.parseCommit(ObjectId.fromString(accountEvent.targetSha));
+      RevCommit current = revWalk.parseCommit(accountRef.getObjectId());
+      return revWalk.isMergedInto(expected, current);
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log(
+          "Could not check whether account %d is up-to-date", accountEvent.accountId);
+      return false;
+    }
+  }
+
+  @Override
+  public boolean isConsistent(Integer accountId) {
+    try (Repository repo = repoManager.openRepository(allUsers)) {
+      return getAccountRef(repo, accountId) != null;
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log(
+          "Could not check whether account %d is consistent", accountId);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Ref getAccountRef(Repository allUsersRepo, int accountId) throws IOException {
+    try (RevWalk revWalk = new RevWalk(allUsersRepo)) {
+      return allUsersRepo.exactRef(RefNames.refsUsers(Account.id(accountId)));
+    }
+  }
+}
