71c1ed1f59cb58f13f7a9d8c4f1e2ca734c4ed68
Luca Milanesio <luca.milanesio@gmail.com>
2026-07-12 18:47:02 +0100

    Abstract the Change consistency into a general ConsistencyChecker interface
    As a preparation work for a generic synchronous acking, allow all
    the checkers to provide information on metadata consistency and
    up-to-date status separately.
    
    For now, implement the ConsistencyChecker only on the ChangeChecker
    interface. The follow-up changes will extend to the other checkers.
    
    Change-Id: I6f353372d1e053da148ecd2fa68db7397d7fe3d6

---
+++ src/main/java/com/gerritforge/gerrit/plugins/multisite/index/ConsistencyChecker.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2025 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.multisite.index;
+
+/**
+ * Interface that allows the multi-site plugin to query for a Git entity consistency before running
+ * its indexing operation and fail.
+ *
+ * @param <K> The entity key type
+ */
+public interface ConsistencyChecker<K> {
+
+  /**
+   * Checks if the local entity's metadata is consistent with its underlying the Git data. Checking
+   * for consistency means:
+   *
+   * <p>- Verifying that the Git repository contains the entity (e.g. the associated ref exists)
+   *
+   * <p>- Verifying that the metadata points to sub-entities that exist on the Git repository (e.g.
+   * the change NoteDb points to existing patch-sets)
+   *
+   * @param entityKey key of the entity to check
+   * @return true if local entity is consistent
+   */
+  boolean isConsistent(K entityKey);
+}
